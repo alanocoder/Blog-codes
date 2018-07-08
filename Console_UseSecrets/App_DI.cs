99e9b784532cf845a36736fb0479738671b98cd4
Alan Chan <alan.online@amulian.com>
2018-07-08 19:22:47 -0400

    use user-secrets in .NET core console app

---
+++ Console_UseSecrets/App_DI.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Console_UseSecrets
+{
+    public class AppSettings
+    {
+        public string Identity { get; set; }
+        public string Secret { get; set; }
+    }
+
+
+    public class App_DI
+    {
+        public static void EnableDependencyInjection(IConfigurationRoot config)
+        {
+            var services = new ServiceCollection();
+            services.Configure<AppSettings>(config.GetSection("AppSettings"));
+            services.AddSingleton<App_DI>();
+
+            var serviceProvider = services.BuildServiceProvider();
+            var app = serviceProvider.GetService<App_DI>(); // obtain an instance of class App.
+            app.Run(); // start running the app
+        }
+
+
+
+        private AppSettings _appSettings;
+        public App_DI(IOptions<AppSettings> options) { this._appSettings = options.Value; }
+        public void Run()
+        {
+            Console.WriteLine($"Identity through dependency injection: {this._appSettings.Identity}");
+        }
+    }
+}
---
+++ Console_UseSecrets/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Console_UseSecrets
+{
+    // This sample shows how to use appSettings and user-secrets at .NET core console application
+    // NuGet packages needed:
+    //  - Microsoft.Extensions.Configuration
+    //  - Microsoft.Extensions.Configuration.Json
+    //  - Microsoft.Extensions.Configuration.UserSecrets
+    //
+    //  or
+    //
+    //  - Microsoft.AspNetCore (this package includes all the needed packages)
+
+
+    // DotNetCliTool to use dotnet user-secrets command (add to .csproj file)
+    // <ItemGroup>
+    //  <DotNetCliToolReference Include = "Microsoft.Extensions.SecretManager.Tools" Version="2.0.0" />
+    // </ItemGroup>
+
+    /* Docker verions to add to docker-compose.override.yml (add a volume to the container so that app can access user-secrets file
+services:
+  console_usesecrets:
+    environment:
+      - DOTNETCORE_ENVIRONMENT=Development
+      - USER_SECRETS_ID=c8cbb18b-d6d2-4bff-81e6-0e0a95f08903
+    volumes:
+      - $APPDATA/Microsoft/UserSecrets/$USER_SECRETS_ID:/root/.microsoft/usersecrets/$USER_SECRETS_ID
+      */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var env = Environment.GetEnvironmentVariable("DOTNETCORE_ENVIRONMENT");
+            if (env == null) env = "Development";
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env}.json", optional: true);
+
+            if (env == "Development") builder.AddUserSecrets<Program>();
+
+            var config = builder.Build();
+            var key = "AppSettings:Identity";
+            Console.WriteLine($"{key} = {config[key]}");
+
+            App_DI.EnableDependencyInjection(config);
+        }
+    }
+}
