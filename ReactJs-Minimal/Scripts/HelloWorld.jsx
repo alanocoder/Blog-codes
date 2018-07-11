4182f458cc78096f4332cb6f15b83e6ef639fe67
Alan Chan <alan.online@amulian.com>
2018-07-11 09:38:10 -0400

    Add ReactJs minimal setup

---
+++ ReactJs-Minimal/Scripts/HelloWorld.jsx
@@ -0,0 +1,7 @@
+﻿import * as React from 'react';
+
+export class HelloWorld extends React.Component {
+    render() {
+        return <div>Hello world using React!</div>;
+    }
+}
---
+++ ReactJs-Minimal/webpack.config.js
@@ -0,0 +1,17 @@
+﻿"use strict";
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    target: 'web',
+    entry: "./Scripts/entry.jsx",
+    output: {
+        path: path.resolve(__dirname, 'wwwroot'),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            { test: /\.jsx$/, loader: 'babel-loader', query: { presets: ['react', ['env', { 'targets': { 'browsers': '> 5%' }}]]}}
+        ]
+    }
+};
