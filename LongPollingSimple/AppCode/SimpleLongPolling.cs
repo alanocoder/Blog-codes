de8da121367d5ac8ecdff6ae2baa16f78ba45d1b
Alan Chan <alan.online@amulian.com>
2018-11-14 08:38:27 -0500

    add LongPollingSimple

---
+++ LongPollingSimple/AppCode/SimpleLongPolling.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LongPollingSimple.AppCode
+{
+    public class SimpleLongPolling
+    {
+        private static List<SimpleLongPolling> _sSubscribers = new List<SimpleLongPolling>();
+
+        public static void Publish(string channel, string message)
+        {
+            lock (_sSubscribers)
+            {
+                var all = _sSubscribers.ToList();
+                foreach (var poll in all)
+                {
+                    if (poll._Channel == channel) poll.Notify(message);
+                }
+            }
+        }
+
+        private TaskCompletionSource<bool> _TaskCompleteion = new TaskCompletionSource<bool>();
+
+        private string _Channel { get; set; }
+        private string _Message { get; set; }
+        public SimpleLongPolling(string channel)
+        {
+            this._Channel = channel;
+            lock (_sSubscribers)
+            {
+                _sSubscribers.Add(this);
+            }
+        }
+
+        private void Notify(string message)
+        {
+            this._Message = message;
+            this._TaskCompleteion.SetResult(true);
+        }
+
+        public async Task<string> WaitAsync()
+        {
+            await Task.WhenAny(_TaskCompleteion.Task, Task.Delay(60000));
+            lock (_sSubscribers)
+            {
+                _sSubscribers.Remove(this);
+            }
+            return this._Message;
+        }
+    }
+}
---
+++ LongPollingSimple/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LongPollingSimple.AppCode;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LongPollingSimple.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> DoLongPolling()
+        {
+            SimpleLongPolling lp = new SimpleLongPolling("sample-channel");
+            var message = await lp.WaitAsync();
+            return new ObjectResult(new { Message = (message != null ? message : "Long polling timeout!") });
+        }
+
+        public IActionResult SimulateTrigger(string message = "Sample publish message")
+        {
+            SimpleLongPolling.Publish("sample-channel", message);
+            return new ObjectResult(new { Message = message, Status = "Published" });
+        }
+    }
+}
